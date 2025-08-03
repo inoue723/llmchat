1ee65fb222287534ed779609f7c285d34c013820
inoue723 <omuni723@gmail.com>
2025-08-03 16:57:30 +0900

    ダークモード対応

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -11,6 +12,22 @@ export function loader({ context }: Route.LoaderArgs) {
   return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
 }
 
+import { useTheme } from 'next-themes'
+
+const ThemeChanger = () => {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div>
+      The current theme is: {theme}
+      <Button className="bg-primary" onClick={() => setTheme('light')}>Light Mode</Button>
+      <Button className="bg-primary" onClick={() => setTheme('dark')}>Dark Mode</Button>
+    </div>
+  )
+}
+
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <div>home</div>
+  return <div>
+    <ThemeChanger />
+  </div>
 }
